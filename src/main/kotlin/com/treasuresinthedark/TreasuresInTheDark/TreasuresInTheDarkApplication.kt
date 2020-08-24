c1025fb8c38bb624341bc83af08ae191426fd28b
Elijah <elijate@gmail.com>
2020-08-24 16:50:05 +0300

    Initial files

---
+++ src/main/kotlin/com/treasuresinthedark/TreasuresInTheDark/TreasuresInTheDarkApplication.kt
@@ -0,0 +1,11 @@
+package com.treasuresinthedark.TreasuresInTheDark
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreasuresInTheDarkApplication
+
+fun main(args: Array<String>) {
+	runApplication<TreasuresInTheDarkApplication>(*args)
+}
